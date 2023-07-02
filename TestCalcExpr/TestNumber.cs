5b9d7c11bd1c0cbd4c214f09d5f7c94793e8b4c6
andrewk17111 <andrewk17111@gmail.com>
2023-07-02 10:33:03 -0400

    Add Number Test
    Added a test for the Number type

---
+++ TestCalcExpr/TestNumber.cs
@@ -0,0 +1,20 @@
+﻿using CalcExpr.Expressions;
+
+namespace TestCalcExpr;
+
+[TestClass]
+public class TestNumber
+{
+    [TestMethod]
+    public void TestInit()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++)
+        {
+            double val = random.NextDouble() * (Double.MaxValue - Double.MinValue) + Double.MinValue;
+
+            Assert.AreEqual(val, new Number(val).Value);
+        }
+    }
+}
