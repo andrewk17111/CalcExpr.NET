9c8da2e7a7d73cd15510ada6e78572f5c16d9e17
andrewk17111 <andrewk17111@gmail.com>
2023-08-20 20:21:02 -0400

    Implement BinaryOperator
    Implemented BinaryOperator except for Evaluation functions

--- CalcExpr/Expressions/BinaryOperator.cs
+++ CalcExpr/Expressions/BinaryOperator.cs
@@ -2,20 +2,19 @@
 
 public class BinaryOperator : IExpression
 {
-    public string Identifier
-        => throw new NotImplementedException();
-
-    public IExpression Left
-        => throw new NotImplementedException();
-
-    public IExpression Right
-        => throw new NotImplementedException();
+    public readonly string Identifier;
+    public readonly IExpression Left;
+    public readonly IExpression Right;
 
     public BinaryOperator(string op, IExpression left, IExpression right)
-        => throw new NotImplementedException();
+    {
+        Identifier = op;
+        Left = left;
+        Right = right;
+    }
 
     public IExpression Clone()
-        => throw new NotImplementedException();
+        => new BinaryOperator(Identifier, Left.Clone(), Right.Clone());
 
     public IExpression Evaluate()
         => throw new NotImplementedException();
@@ -24,14 +23,15 @@ public class BinaryOperator : IExpression
         => throw new NotImplementedException();
 
     public override bool Equals(object? obj)
-        => throw new NotImplementedException();
+        => obj is not null && obj is BinaryOperator bin_op && bin_op.Identifier == Identifier &&
+            bin_op.Left.Equals(Left) && bin_op.Right.Equals(Right);
 
     public override int GetHashCode()
-        => throw new NotImplementedException();
+        => Identifier.GetHashCode();
 
     public override string ToString()
-        => throw new NotImplementedException();
+        => ToString(null);
 
     public string ToString(string? format)
-        => throw new NotImplementedException();
+        => $"{Left.ToString(format)}{Identifier}{Right.ToString(format)}";
 }
