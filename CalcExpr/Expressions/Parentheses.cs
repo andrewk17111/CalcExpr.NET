ccd429183559a8cb6e7bccf3a17307d07302fcac
andrewk17111 <andrewk17111@gmail.com>
2023-08-24 09:23:06 -0400

    Create Parentheses
    Created a Parentheses class to be used for expressions wrapped around Parentheses to determine order of operations

---
+++ CalcExpr/Expressions/Parentheses.cs
@@ -0,0 +1,34 @@
+﻿namespace CalcExpr.Expressions;
+
+public class Parentheses : IExpression
+{
+    public readonly IExpression Inside;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Parentheses"/> class.
+    /// </summary>
+    /// <param name="inside">The <see cref="IExpression"/> inside of this <see cref="Parentheses"/>.</param>
+    public Parentheses(IExpression inside)
+        => Inside = inside;
+
+    public IExpression Clone()
+        => throw new NotImplementedException();
+
+    public IExpression Evaluate()
+        => throw new NotImplementedException();
+
+    public IExpression StepEvaluate()
+        => throw new NotImplementedException();
+
+    public override bool Equals(object? obj)
+        => throw new NotImplementedException();
+
+    public override int GetHashCode()
+        => throw new NotImplementedException();
+
+    public override string ToString()
+        => ToString(null);
+
+    public string ToString(string? format)
+        => throw new NotImplementedException();
+}
