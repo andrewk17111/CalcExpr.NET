f802a1fdc70f0d675f1b6f1e54d7932b35e9bc3e
andrewk17111 <andrewk17111@gmail.com>
2023-08-31 14:58:55 -0400

    Add Constant
    Added new Constant type and added tests for Constants in TestConstant along with test cases for Constants in TestParser

---
+++ CalcExpr/Expressions/Constant.cs
@@ -0,0 +1,30 @@
+﻿namespace CalcExpr.Expressions;
+
+public class Constant : IExpression
+{
+    public readonly string Identifier;
+
+    public Constant(string identifier)
+        => throw new NotImplementedException();
+
+    public IExpression Clone()
+        => throw new NotImplementedException();
+
+    public IExpression Evaluate()
+        => throw new NotImplementedException();
+
+    public IExpression StepEvaluate()
+        => throw new NotImplementedException();
+
+    public override bool Equals(object? obj)
+        => throw new NotImplementedException();
+
+    public override int GetHashCode()
+        => throw new NotImplementedException();
+
+    public override string ToString()
+        => throw new NotImplementedException();
+
+    public string ToString(string? format)
+        => throw new NotImplementedException();
+}
---
+++ TestCalcExpr/TestConstant.cs
@@ -0,0 +1,65 @@
+﻿using CalcExpr.Expressions;
+
+namespace TestCalcExpr;
+
+[TestClass]
+public class TestConstant
+{
+    readonly string[] CONSTANTS =
+    {
+        "∞",
+        "inf",
+        "infinity",
+        "π",
+        "pi",
+        "τ",
+        "tau",
+        "e",
+        "true",
+        "false",
+    };
+
+    /// <summary>
+    /// Tests that the value of the Constant is the same as the value passed into the Constant's constructor.
+    /// </summary>
+    [TestMethod]
+    public void TestInit()
+    {
+        foreach (string constant in CONSTANTS)
+            Assert.AreEqual(constant, new Constant(constant).Identifier);
+    }
+
+    /// <summary>
+    /// Tests that the Constant gets evaluated into the proper value.
+    /// </summary>
+    [TestMethod]
+    public void TestEvaluate()
+    {
+        Dictionary<Constant, IExpression> constants = new Dictionary<Constant, IExpression>()
+        {
+            { new Constant("∞"), new Constant("∞") },
+            { new Constant("inf"), new Constant("inf") },
+            { new Constant("infinity"), new Constant("infinity") },
+            { new Constant("π"), new Number(Math.PI) },
+            { new Constant("pi"), new Number(Math.PI) },
+            { new Constant("τ"), new Number(Math.Tau) },
+            { new Constant("tau"), new Number(Math.Tau) },
+            { new Constant("e"), new Number(Math.E) },
+            { new Constant("true"), new Number(1) },
+            { new Constant("false"), new Number(0) },
+        };
+
+        foreach (Constant constant in constants.Keys)
+            Assert.AreEqual(constants[constant], constant.Evaluate());
+    }
+
+    /// <summary>
+    /// Tests that the Constant converts to a string properly.
+    /// </summary>
+    [TestMethod]
+    public void TestToString()
+    {
+        foreach (string constant in CONSTANTS)
+            Assert.AreEqual(constant, new Constant(constant).ToString());
+    }
+}
