cffb8f95339f943c19a907aa9b89b21873331de4
andrewk17111 <andrewk17111@gmail.com>
2024-03-18 18:07:21 -0400

    Added Statistical Functions
    Added statistical functions and corresponding test cases

---
+++ CalcExpr/FunctionAttributes/ConditionalAttributes/AreNumbersAttribute.cs
@@ -0,0 +1,21 @@
+﻿using CalcExpr.Expressions;
+using CalcExpr.Expressions.Collections;
+
+namespace CalcExpr.FunctionAttributes.ConditionalAttributes;
+
+public class AreNumbersAttribute : ConditionAttribute
+{
+    public override bool CheckCondition(IExpression expression)
+    {
+        if (expression is IEnumerableExpression enum_expr)
+        {
+            foreach (IExpression expr in enum_expr)
+                if (expr is not Number)
+                    return false;
+
+            return true;
+        }
+
+        return expression is Number;
+    }
+}
