24e0979183087828ae5471e3749636aacebd1308
andrewk17111 <andrewk17111@gmail.com>
2024-03-28 20:21:36 -0400

    Implemented Map and Combine
    Implemented the map and combine functions

--- CalcExpr/Expressions/Collections/Set.cs
+++ CalcExpr/Expressions/Collections/Set.cs
@@ -5,7 +5,7 @@ namespace CalcExpr.Expressions.Collections;
 
 public class Set(IEnumerable<IExpression> elements) : IEnumerableExpression
 {
-    private readonly HashSet<IExpression> _elements = elements.ToHashSet();
+    private readonly SortedSet<IExpression> _elements = [.. elements];
 
     public int Count
         => _elements.Count;
@@ -23,21 +23,32 @@ public class Set(IEnumerable<IExpression> elements) : IEnumerableExpression
         => throw new NotImplementedException();
 
     public IEnumerableExpression Map(Func<IExpression, IExpression> map)
-        => throw new NotImplementedException();
+        => new Set(_elements.Select(x => map(x)));
 
     public IEnumerableExpression Combine(IEnumerable<IExpression> expressions,
         Func<IExpression, IExpression, IExpression> combine)
-        => throw new NotImplementedException();
+    {
+        expressions = expressions.OrderBy(x => x.GetHashCode());
+
+        IEnumerable<IExpression> elements = _elements.OrderBy(x => x.GetHashCode());
+        List<IExpression> result = [];
+        int count = expressions.Count();
+
+        for (int i = 0; i < Count && i < count; i++)
+            result.Add(combine(elements.ElementAt(i), expressions.ElementAt(i)));
+
+        return new Set(result);
+    }
 
     public static IEnumerableExpression ConvertIEnumerable(IEnumerable<IExpression> elements)
         => new Set(elements);
 
     public override bool Equals(object? obj)
     {
-        if (obj is not null && obj is Vector vect)
+        if (obj is not null && obj is Set set)
         {
-            bool elements_equal = vect.Length == Count &&
-                (Count == 0 || !vect.Select((arg, i) => arg.Equals(this.ElementAt(i))).Any(x => !x));
+            bool elements_equal = set.Count == Count &&
+                (Count == 0 || !set.Select((arg, i) => arg.Equals(this.ElementAt(i))).Any(x => !x));
 
             return elements_equal;
         }
