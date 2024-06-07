e61c4271d61a64f847b6349a0c0cf15f6041ffbe
andrewk17111 <andrewk17111@gmail.com>
2024-06-07 14:58:53 -0400

    Added TypeConverters
    Added TypeConverters that can convert between compatible types and IExpression

---
+++ CalcExpr/TypeConverters/BooleanTypeConverter.cs
@@ -0,0 +1,27 @@
+﻿using CalcExpr.Expressions;
+using CalcExpr.FunctionAttributes.PreprocessAttributes;
+
+namespace CalcExpr.TypeConverters;
+
+public class BooleanTypeConverter : ITypeConverter<bool?>
+{
+    public IExpression ConvertToExpression(bool? value)
+    {
+        if (value.HasValue)
+        {
+            return value.Value ? Constant.TRUE : Constant.FALSE;
+        }
+
+        return Constant.UNDEFINED;
+    }
+
+    public bool? ConvertFromExpression(IExpression expression)
+    {
+        IExpression result = new AsBooleanAttribute().Preprocess(expression);
+
+        if (Constant.UNDEFINED.Equals(result))
+            return null;
+
+        return Constant.TRUE.Equals(result);
+    }
+}
