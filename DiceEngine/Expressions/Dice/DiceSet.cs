9c8332ad695ce343b705cabfbc95183855d6af12
whix100 <whix100@gmail.com>
2024-06-09 18:43:49 -0400

    Created DiceSet
    Created a DiceSet to contain multiple dice to roll

---
+++ DiceEngine/Expressions/Dice/DiceSet.cs
@@ -0,0 +1,36 @@
+﻿using DiceEngine.Context;
+
+namespace DiceEngine.Expressions.Dice;
+
+public class DiceSet(int size, IDie die) : IDie
+{
+    public readonly int Size = size;
+    public readonly IDie Die = die;
+
+    public IExpression Evaluate()
+        => Evaluate(new ExpressionContext());
+
+    public IExpression Evaluate(ExpressionContext context)
+        => new RollResult(Enumerable.Range(1, Size).Select(x => Roll(context.Random)), this);
+
+    public IExpression StepEvaluate()
+        => StepEvaluate(new ExpressionContext());
+
+    public IExpression StepEvaluate(ExpressionContext context)
+        => Evaluate(context);
+
+    public Number Roll(Random? random = null)
+        => Die.Roll();
+
+    public override int GetHashCode()
+        => Size;
+
+    public override bool Equals(object? obj)
+        => obj is DiceSet diceSet && diceSet.Size == Size && diceSet.Die.Equals(Die);
+
+    public override string ToString()
+        => ToString(null);
+
+    public string ToString(string? format)
+        => $"{Size}{Die.ToString(format)}";
+}
---
+++ TestDiceEngine/Expressions/Dice/TestDiceSet.cs
@@ -0,0 +1,73 @@
+﻿using DiceEngine.Expressions.Dice;
+using System.Drawing;
+
+namespace TestDiceEngine.Expressions.Dice;
+
+[TestClass]
+public class TestDiceSet
+{
+    /// <summary>
+    /// Tests that the value of the DiceSet is the same as the value passed into the DiceSet's constructor.
+    /// </summary>
+    [TestMethod]
+    public void TestInit()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++)
+        {
+            int size = random.Next(1, 100);
+            int setSize = random.Next(1, 100);
+            Die die = new Die(size);
+            DiceSet diceSet = new DiceSet(setSize, die);
+
+            Assert.AreEqual(setSize, diceSet.Size);
+            Assert.AreEqual(die, diceSet.Die);
+        }
+
+        int percentileSetSize = random.Next(1, 100);
+        PercentileDie percentileDie = new PercentileDie();
+        DiceSet percentileDiceSet = new DiceSet(percentileSetSize, percentileDie);
+
+        Assert.AreEqual(percentileSetSize, percentileDiceSet.Size);
+        Assert.AreEqual(percentileDie, percentileDiceSet.Die);
+
+        int fateSetSize = random.Next(1, 100);
+        FateDie fateDie = new FateDie();
+        DiceSet fateDiceSet = new DiceSet(fateSetSize, fateDie);
+
+        Assert.AreEqual(fateSetSize, fateDiceSet.Size);
+        Assert.AreEqual(fateDie, fateDiceSet.Die);
+    }
+
+    /// <summary>
+    /// Tests that the DiceSet converts to a string properly.
+    /// </summary>
+    [TestMethod]
+    public void TestToString()
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < 100; i++)
+        {
+            int size = random.Next(1, 100);
+            int setSize = random.Next(1, 100);
+            Die die = new Die(size);
+            DiceSet diceSet = new DiceSet(setSize, die);
+
+            Assert.AreEqual($"{setSize}d{size}", diceSet.ToString());
+        }
+
+        int percentileSetSize = random.Next(1, 100);
+        PercentileDie percentileDie = new PercentileDie();
+        DiceSet percentileDiceSet = new DiceSet(percentileSetSize, percentileDie);
+
+        Assert.AreEqual($"{percentileSetSize}d%", percentileDiceSet.ToString());
+
+        int fateSetSize = random.Next(1, 100);
+        FateDie fateDie = new FateDie();
+        DiceSet fateDiceSet = new DiceSet(fateSetSize, fateDie);
+
+        Assert.AreEqual($"{fateSetSize}dF", fateDiceSet.ToString());
+    }
+}
