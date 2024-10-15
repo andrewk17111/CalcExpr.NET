cc71d7fc7ee8e07bd649df0182ed8c08fc538e58
andrewk17111 <andrewk17111@gmail.com>
2024-10-15 09:39:26 -0400

    Created Terminal

---
+++ CalcExpr/Expressions/Terminals/Terminal.cs
@@ -0,0 +1,23 @@
+﻿using CalcExpr.Context;
+
+namespace CalcExpr.Expressions.Terminals;
+
+public abstract class Terminal : IExpression
+{
+    public IExpression Evaluate()
+        => this;
+
+    public IExpression Evaluate(ExpressionContext _)
+        => this;
+
+    public IExpression StepEvaluate()
+        => this;
+
+    public IExpression StepEvaluate(ExpressionContext _)
+        => this;
+
+    public override string ToString()
+        => ToString(null);
+
+    public abstract string ToString(string? format);
+}
